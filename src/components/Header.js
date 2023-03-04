2a28a87e49d2dbd6acba3d3f30c12faf4f2bfea1
TatsuKenshi <rockinbg@gmail.com>
2023-03-04 18:47:10 +0100

    adds jsx to all component files

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,7 +1,46 @@
 import React from "react";
+import { Link } from "react-router-dom";
+import { ShoppingBasket, SearchOutlined } from "@mui/icons-material";
+import "../styles/Header.scss";
 
 const Header = () => {
-  return <div>Header</div>;
+  return (
+    <div className="header">
+      <nav>
+        <ul className="ul-items">
+          <li className="li-items">
+            <Link to="/women">Women</Link>
+            <Link to="/men">Men</Link>
+            <Link to="/denim">Denim</Link>
+            <Link to="/shop" className="orange">
+              Gift Shop
+            </Link>
+            <Link to="/about">About</Link>
+          </li>
+        </ul>
+
+        <div className="logo">
+          <h1>Eshopland</h1>
+        </div>
+
+        <div className="right">
+          <div className="search">
+            <SearchOutlined className="search-icon" />
+            <button className="sch-btn">Search</button>
+          </div>
+          <div className="right-buttons">
+            <button>
+              <Link to="/login">Login</Link>
+            </button>
+            <button>
+              <Link to="/signup">Sign Up</Link>
+            </button>
+            <ShoppingBasket className="basket" />
+          </div>
+        </div>
+      </nav>
+    </div>
+  );
 };
 
 export default Header;
--- src/components/Section.js
+++ src/components/Section.js
@@ -1,7 +1,19 @@
 import React from "react";
+import "../styles/Section.scss";
 
 const Section = () => {
-  return <div>Section</div>;
+  return (
+    <section className="main-section">
+      <div className="content">
+        <h1>Be Warm, Merry and Stand Out.</h1>
+        <button>Discover</button>
+      </div>
+
+      <div className="bottom-text">
+        <p>10% off for all products</p>
+      </div>
+    </section>
+  );
 };
 
 export default Section;
