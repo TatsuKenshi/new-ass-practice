44f37acf580f3215403535d73c8bf8438458d474
TatsuKenshi <rockinbg@gmail.com>
2023-03-04 18:42:49 +0100

    adds jsx and imports to all page files and App.js

--- src/App.js
+++ src/App.js
@@ -1,7 +1,23 @@
 import "./App.scss";
+import Header from "./components/Header";
+import Section from "./components/Section";
+import SmallSection from "./components/SmallSection";
+import CreamSection from "./components/CreamSection";
+import Carousel from "./components/Carousel";
+import Footer from "./components/Footer";
+import { Data } from "./components/Data";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <Header />
+      <Section />
+      <SmallSection />
+      <CreamSection />
+      <Carousel slides={Data} />
+      <Footer />
+    </div>
+  );
 }
 
 export default App;
--- src/pages/Error.js
+++ src/pages/Error.js
@@ -1,7 +1,11 @@
 import React from "react";
 
 const Error = () => {
-  return <div>Error</div>;
+  return (
+    <div className="error-page">
+      <h1>Error. This page doesn't exist</h1>
+    </div>
+  );
 };
 
 export default Error;
